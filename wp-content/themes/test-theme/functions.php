a6feec108b8ac82b5c4836a72504825a3d48b678
Cesar <me@cesarvaca.dev>
2024-05-10 21:25:33 +1000

    Added new theme

---
+++ wp-content/themes/test-theme/functions.php
@@ -0,0 +1,108 @@
+<?php
+
+
+function load_scripts()
+{
+
+	wp_enqueue_script('jquery');
+	wp_enqueue_style('main_stylesheet', get_template_directory_uri() . '/style.css', array(), 1, 'all');
+	wp_enqueue_script('main_js', get_template_directory_uri() . '/custom.js', array('jquery'), 1, true);
+
+}
+
+add_action('wp_enqueue_scripts', 'load_scripts');
+
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function customtheme_setup()
+{
+
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support('automatic-feed-links');
+
+	/*
+		* Let WordPress manage the document title.
+		* By adding theme support, we declare that this theme does not use a
+		* hard-coded <title> tag in the document head, and expect WordPress to
+		* provide it for us.
+		*/
+	add_theme_support('title-tag');
+
+	/*
+		* Enable support for Post Thumbnails on posts and pages.
+		*
+		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		*/
+	add_theme_support('post-thumbnails');
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus(
+		array(
+			'menu-1' => esc_html__('Primary', 'customtheme'),
+		)
+	);
+
+	/*
+		* Switch default core markup for search form, comment form, and comments
+		* to output valid HTML5.
+		*/
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		)
+	);
+}
+add_action('after_setup_theme', 'customtheme_setup');
+
+
+
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css()
+{
+	wp_dequeue_style('wp-block-library');
+	wp_dequeue_style('wp-block-library-theme');
+	wp_dequeue_style('wc-blocks-style'); // Remove WooCommerce block CSS
+}
+add_action('wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100);
+
+
+
+// Disable Emojis
+function disable_emojis()
+{
+	remove_action('wp_head', 'print_emoji_detection_script', 7);
+	remove_action('admin_print_scripts', 'print_emoji_detection_script');
+	remove_action('wp_print_styles', 'print_emoji_styles');
+	remove_action('admin_print_styles', 'print_emoji_styles');
+	remove_filter('the_content_feed', 'wp_staticize_emoji');
+	remove_filter('comment_text_rss', 'wp_staticize_emoji');
+	remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+
+	// Remove from TinyMCE
+	add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
+}
+
+function disable_emojis_tinymce($plugins)
+{
+	if (is_array($plugins)) {
+		return array_diff($plugins, array('wpemoji'));
+	} else {
+		return array();
+	}
+}
+
+add_action('init', 'disable_emojis');
