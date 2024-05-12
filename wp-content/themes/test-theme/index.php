cc8e347f25d356a4f75914060edff3e8129a279c
Cesar <me@cesarvaca.dev>
2024-05-12 11:22:16 +1000

    New stuff

--- wp-content/themes/test-theme/index.php
+++ wp-content/themes/test-theme/index.php
@@ -1,6 +1,8 @@
 <?php get_header();?>
 
-<?php if (have_posts()): while (have_posts()): the_post();?>      
+<?php if (have_posts()): while (have_posts()): the_post();?>   
+
+<h1>This is a testing</h1>
 
 <?php the_content();?>
 
